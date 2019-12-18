ff2f1bad81dec9fbf7154b7e26dde5125d5438b1
admin <priyankamunjane@gnail.com>
2019-12-18 16:49:02 +0530

    commit

---
+++ AssignmentAndroidProject/app/src/main/java/com/assignment/model/ResponseModel.java
@@ -0,0 +1,34 @@
+
+package com.assignment.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class ResponseModel {
+
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("rows")
+    @Expose
+    private List<SingleResponseModel> rows = null;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<SingleResponseModel> getRows() {
+        return rows;
+    }
+
+    public void setRows(List<SingleResponseModel> rows) {
+        this.rows = rows;
+    }
+
+}
